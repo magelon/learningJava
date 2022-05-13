475d930f0794630e7cd03b95f7605eb10bef5155
magelon <alfardproject@gmail.com>
2022-05-12 20:33:21 -0700

    printed bb

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,8 @@
 public class Main {
     public static void main(String[] args){
 
-
-
-
+    battleBoard bb=new battleBoard(3,3);
+    bb.buildBattleBoard();
+    bb.drawBoard();
     }
-
-
 }
--- src/battleBoard.java
+++ src/battleBoard.java
@@ -1,27 +1,78 @@
 import java.util.Arrays;
+import java.util.Vector;
 
 public class battleBoard {
-    public battleBoard(){}
+    private int boardLength;
 
-    private void buildBattleBoard(){
-        //creat a battleBoard
-        String[][] battleBoard=new String[10][10];
+    private int boardWidth;
+
+    private String[][] battleBoard;
+
+    //battleBoard constructor
+    public battleBoard(int boardLength,int boardWidth){
+        this.boardLength=boardLength;
+        this.boardWidth=boardWidth;
+        this.battleBoard=new String[boardLength][boardWidth];
+    }
+
+    public int getBoardLength() {
+        return boardLength;
+    }
+
+    public void setBoardLength(int boardLength) {
+        this.boardLength = boardLength;
+    }
+
+    public int getBoardWidth() {
+        return boardWidth;
+    }
+
+    public void setBoardWidth(int boardWidth) {
+        this.boardWidth = boardWidth;
+    }
+
+    public void buildBattleBoard(){
         //fillup board
-        //creat monster array
-        Monster[] monsters=new Monster[5];
+        /*
+        for(int i=0;i<boardLength;i++){
+            for(int j=0;j<boardWidth;j++){
+                battleBoard[i][j]="*";
+            }
+        }
+        */
+         for(int i=0;i<boardLength;i++){
+             Arrays.fill(battleBoard[i],"*");
+             }
+
+
+
         //randomlly put monsters on the board
-        //draw the board
+    }
+    public void buildBattleBoard(Vector<Monster> monsters){
+        //fillup board
+
+        //Arrays.fill(battleBoard,'*');
+        //randomlly put monsters on the board
+    }
+
+    public void drawBoard(){
         int k=1;
-        while(k<=30){
+        while(k<=this.boardLength*3){
             System.out.print('_');
             k++;
         }
-
-
-    }
-    private static void fillUpArray(String[][] battleBoard){
-        for (String[] strings : battleBoard) {
-            Arrays.fill(strings, "*");
+        System.out.println();
+        for(int i=0;i<this.boardLength;i++){
+            for(int j=0;j<this.boardWidth;j++){
+                System.out.print('|'+this.battleBoard[i][j]+'|');
+            }
+            System.out.println();
+        }
+        int r=1;
+        while(r<=this.boardLength*3){
+            System.out.print('_');
+            r++;
         }
     }
+
 }
