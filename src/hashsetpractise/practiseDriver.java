85ea93ad8c33d378591cc4669541a0382ecbd460
magelon <alfardproject@gmail.com>
2022-05-26 16:29:34 -0700

    printed bb

---
+++ src/hashsetpractise/practiseDriver.java
@@ -0,0 +1,11 @@
+package hashsetpractise;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class practiseDriver {
+    public static void main(String args[]){
+
+        Set<Integer> s=new HashSet();
+    }
+}
